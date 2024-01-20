5531c9000150db94f23e406f496d7152e628c919
Aravind Venkatesan <43500365+aravinve@users.noreply.github.com>
2024-01-20 21:30:31 +0800

    Add basic endpoints

--- main.py
+++ main.py
@@ -1,14 +1,45 @@
+import os
+from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 from src.routes import qrcode
+from datetime import datetime
+
+load_dotenv()
 
 app = FastAPI()
 
-app.include_router(qrcode.router, prefix="/qrcode", tags=["qrcode"])
+app.include_router(qrcode.router, prefix="/v1/qrcode", tags=["qrcode"])
 
-@app.get("/")
-def read_root():
-    return {"message": "Welcome to the QR Code Generator API"}
+server_start_time = datetime.now()
+
+@app.get("/", response_class=JSONResponse)
+def root():
+    return {
+        "serviceName": "FastApi Python Service", 
+        "message": "Welcome to the Fast API Backend",
+        "author": "Aravind Venkatesan",
+        "repository": "https://github.com/aravinve/fastapi-backend"
+    }
+
+@app.get("/health", response_class=JSONResponse)
+def check_health():
+    return {"status": "OK"}
+
+@app.get("/readiness", response_class=JSONResponse)
+def check_readiness():
+    # Add your custom readiness checks here
+    # For example, check if your application dependencies are available
+    # If everything is ready, return {"status": "OK"}
+    return {"status": "OK"}
+
+@app.get("/up", response_class=JSONResponse)
+def get_server_uptime():
+    server_uptime = datetime.now() - server_start_time
+    return {"status": "OK", "uptime": str(server_uptime)}
 
 if __name__ == "__main__":
+    host = os.getenv("FASTAPI_HOST", "127.0.0.1")
+    port = int(os.getenv("FASTAPI_PORT", 8000))
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host, port)
